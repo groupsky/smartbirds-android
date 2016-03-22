c12abfe025700887c0203f1f6079a1187d297bfd
Geno Roupsky <geno@roupsky.name>
2016-03-22 16:42:09 +0200

    use specific date/time format for csv

---
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/views/SupportStorage.java
@@ -0,0 +1,10 @@
+package org.bspb.smartbirds.pro.ui.views;
+
+/**
+ * Created by groupsky on 22.03.16.
+ */
+public interface SupportStorage {
+    String valueForStorage();
+
+    void restoreFromStorage(String value);
+}
