d0e90118a01e90cf95d55237c57cb0470d4cdd9e
Geno Roupsky <geno@roupsky.name>
2016-03-22 17:31:42 +0200

    observers defaults to last used

---
+++ app/src/main/java/org/bspb/smartbirds/pro/prefs/CommonPrefs.java
@@ -0,0 +1,12 @@
+package org.bspb.smartbirds.pro.prefs;
+
+import org.androidannotations.annotations.sharedpreferences.SharedPref;
+
+/**
+ * Created by groupsky on 22.03.16.
+ */
+@SharedPref
+public interface CommonPrefs {
+
+    String commonObservers();
+}
