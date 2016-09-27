6ea1b2dd3e76da8e210a926f4fb9b468f48138fe
Geno Roupsky <geno@roupsky.name>
2016-09-27 16:37:45 +0300

    handle single/multi choice fields to correctly send to server

--- app/src/main/java/org/bspb/smartbirds/pro/ui/views/DateFormInput.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/views/DateFormInput.java
@@ -2,26 +2,22 @@ package org.bspb.smartbirds.pro.ui.views;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
-import android.content.res.TypedArray;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.DatePicker;
-import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 
 import org.bspb.smartbirds.pro.R;
 import org.bspb.smartbirds.pro.SmartBirdsApplication;
-import org.bspb.smartbirds.pro.ui.exception.ViewValidationException;
 import org.bspb.smartbirds.pro.ui.utils.Configuration;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by groupsky on 14-10-17.
@@ -67,13 +63,15 @@ public class DateFormInput extends TextViewFormInput implements SupportStorage {
     }
 
     @Override
-    public String valueForStorage() {
-        return mValue != null ? mStorageFormat.format(mValue.getTime()) : "";
+    public void serializeToStorage(Map<String, String> storage, String fieldName) {
+        storage.put(fieldName, mValue != null ? mStorageFormat.format(mValue.getTime()) : "");
     }
 
     @Override
-    public void restoreFromStorage(String value) {
+    public void restoreFromStorage(Map<String, String> storage, String fieldName) {
         try {
+            final String value = storage.get(fieldName);
+            if (TextUtils.isEmpty(value)) return;
             final Calendar calendar = Calendar.getInstance();
             calendar.setTime(mStorageFormat.parse(value));
             setValue(calendar);
@@ -86,7 +84,7 @@ public class DateFormInput extends TextViewFormInput implements SupportStorage {
     private class PopupDialog implements DatePickerDialog.OnDateSetListener {
 
         public void show() {
-            final Calendar c = mValue!=null?mValue:Calendar.getInstance();
+            final Calendar c = mValue != null ? mValue : Calendar.getInstance();
             final int year = c.get(Calendar.YEAR);
             final int month = c.get(Calendar.MONTH);
             final int day = c.get(Calendar.DAY_OF_MONTH);
--- app/src/main/java/org/bspb/smartbirds/pro/ui/views/MultipleChoiceFormInput.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/views/MultipleChoiceFormInput.java
@@ -6,21 +6,21 @@ import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EView;
 import org.bspb.smartbirds.pro.R;
-import org.bspb.smartbirds.pro.SmartBirdsApplication;
 import org.bspb.smartbirds.pro.backend.dto.Nomenclature;
 import org.bspb.smartbirds.pro.ui.utils.Configuration;
 import org.bspb.smartbirds.pro.ui.utils.NomenclaturesBean;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 import static org.bspb.smartbirds.pro.ui.utils.Configuration.MULTIPLE_CHOICE_SPLITTER;
 
@@ -28,9 +28,10 @@ import static org.bspb.smartbirds.pro.ui.utils.Configuration.MULTIPLE_CHOICE_SPL
  * Created by groupsky on 14-10-13.
  */
 @EView
-public class MultipleChoiceFormInput extends TextViewFormInput {
+public class MultipleChoiceFormInput extends TextViewFormInput implements SupportStorage {
 
     CharSequence[] entries;
+    Nomenclature[] items;
 
     private final CharSequence key;
     @Bean
@@ -83,30 +84,52 @@ public class MultipleChoiceFormInput extends TextViewFormInput {
         return true;
     }
 
+    void prepareSelected() {
+        if (entries == null && key != null) {
+            List<Nomenclature> values = nomenclatures.getNomenclature(key.toString());
+            if (values != null) {
+                entries = new CharSequence[values.size()];
+                items = new Nomenclature[values.size()];
+                mSelected = new boolean[values.size()];
+                for (int i = 0; i < values.size(); i++) {
+                    items[i] = values.get(i);
+                    entries[i] = values.get(i).localeLabel;
+                }
+
+                String[] selected = getText().toString().split(MULTIPLE_CHOICE_SPLITTER);
+                Arrays.sort(selected);
+                for (int idx=0; idx < entries.length; idx++) {
+                    mSelected[idx] = Arrays.binarySearch(selected, entries[idx]) == 0;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void serializeToStorage(Map<String, String> storage, String fieldName) {
+        storage.put(fieldName, getText().toString());
+        prepareSelected();
+        List<Nomenclature> selectedItems = new LinkedList<>();
+        for (int idx=0; idx<mSelected.length; idx++) {
+            if (mSelected[idx]) {
+                selectedItems.add(items[idx]);
+            }
+        }
+        storage.put(fieldName+".json", new Gson().toJson(selectedItems));
+    }
+
+    @Override
+    public void restoreFromStorage(Map<String, String> storage, String fieldName) {
+        setText(storage.get(fieldName));
+    }
+
     private class PopupDialog implements DialogInterface.OnClickListener, DialogInterface.OnMultiChoiceClickListener {
 
         private AlertDialog mPopup;
         boolean[] selected;
 
         public void show() {
-            if (entries == null && key != null) {
-                List<Nomenclature> values = nomenclatures.getNomenclature(key.toString());
-                if (values != null) {
-                    entries = new CharSequence[values.size()];
-                    mSelected = new boolean[values.size()];
-                    for (int i = 0; i < values.size(); i++) {
-                        entries[i] = values.get(i).localeLabel;
-                    }
-
-                    String[] items = getText().toString().split(MULTIPLE_CHOICE_SPLITTER);
-                    for (String item : items) {
-                        if (values.contains(item)) {
-                            mSelected[values.indexOf(item)] = true;
-                        }
-                    }
-                }
-            }
-
+            prepareSelected();
 
             this.selected = mSelected.clone();
             mPopup = new AlertDialog.Builder(getContext())
