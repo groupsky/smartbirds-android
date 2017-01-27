4a298d982c9b9742533faa0237132666607acb4d
Geno Roupsky <geno@roupsky.name>
2017-01-27 15:35:28 +0200

    refactor images so it can be embeeded in different places

--- app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/NewCiconiaEntryFormFragment.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/NewCiconiaEntryFormFragment.java
@@ -1,20 +1,62 @@
 package org.bspb.smartbirds.pro.ui.fragment;
 
 import android.app.Fragment;
+import android.os.Build;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.FragmentById;
 import org.bspb.smartbirds.pro.R;
 import org.bspb.smartbirds.pro.enums.EntryType;
+
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Created by dani on 14-11-11.
  */
 @EFragment(R.layout.fragment_monitoring_form_new_ciconia_entry)
+
 public class NewCiconiaEntryFormFragment extends BaseEntryFragment {
+
+    @FragmentById(R.id.pictures_fragment)
+    NewEntryPicturesFragment picturesFragment;
+    private HashMap<String, String> pendingDeserialize;
 
     @Override
     protected EntryType getEntryType() {
         return EntryType.CICONIA;
+    }
+
+    @Override
+    protected HashMap<String, String> serialize(Date entryTime) {
+        HashMap<String, String> data = super.serialize(entryTime);
+        data.putAll(picturesFragment.serialize());
+        return data;
+    }
+
+    @Override
+    protected void deserialize(HashMap<String, String> data) {
+        super.deserialize(data);
+        if (picturesFragment == null) {
+            pendingDeserialize = data;
+        } else {
+            picturesFragment.deserialize(data);
+        }
+    }
+
+    @AfterViews
+    protected void flushDeserialize() {
+        if (picturesFragment == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            picturesFragment = (NewEntryPicturesFragment) getChildFragmentManager().findFragmentById(R.id.pictures_fragment);
+        }
+        if (picturesFragment == null) {
+            picturesFragment = (NewEntryPicturesFragment) getFragmentManager().findFragmentById(R.id.pictures_fragment);
+        }
+        if (pendingDeserialize != null) {
+            picturesFragment.deserialize(pendingDeserialize);
+            pendingDeserialize = null;
+        }
     }
 
     public static class Builder implements BaseEntryFragment.Builder {
--- app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/NewHerpEntryRequiredFormFragment.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/NewHerpEntryRequiredFormFragment.java
@@ -1,12 +1,18 @@
 package org.bspb.smartbirds.pro.ui.fragment;
 
+import android.os.Build;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.FragmentById;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 import org.bspb.smartbirds.pro.R;
 import org.bspb.smartbirds.pro.prefs.HerpPrefs_;
 import org.bspb.smartbirds.pro.ui.views.DecimalNumberFormInput;
 import org.bspb.smartbirds.pro.ui.views.SingleChoiceFormInput;
+
+import java.util.HashMap;
 
 /**
  * Created by groupsky on 26.01.17.
@@ -24,10 +30,45 @@ public class NewHerpEntryRequiredFormFragment extends BaseFormFragment {
     @Pref
     HerpPrefs_ prefs;
 
+    @FragmentById(R.id.pictures_fragment)
+    NewEntryPicturesFragment picturesFragment;
+    private HashMap<String, String> pendingDeserialize;
+
     @Override
     public void onResume() {
         super.onResume();
         habitat.setText(prefs.herpHabitat().get());
+    }
+
+    @Override
+    protected HashMap<String, String> serialize() {
+        HashMap<String, String> data = super.serialize();
+        data.putAll(picturesFragment.serialize());
+        return data;
+    }
+
+    @Override
+    protected void deserialize(HashMap<String, String> data) {
+        super.deserialize(data);
+        if (picturesFragment == null) {
+            pendingDeserialize = data;
+        } else {
+            picturesFragment.deserialize(data);
+        }
+    }
+
+    @AfterViews
+    protected void flushDeserialize() {
+        if (picturesFragment == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            picturesFragment = (NewEntryPicturesFragment) getChildFragmentManager().findFragmentById(R.id.pictures_fragment);
+        }
+        if (picturesFragment == null) {
+            picturesFragment = (NewEntryPicturesFragment) getFragmentManager().findFragmentById(R.id.pictures_fragment);
+        }
+        if (pendingDeserialize != null) {
+            picturesFragment.deserialize(pendingDeserialize);
+            pendingDeserialize = null;
+        }
     }
 
     @Override
