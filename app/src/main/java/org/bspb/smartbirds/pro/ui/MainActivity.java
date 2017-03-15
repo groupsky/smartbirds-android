f2ba2670d81eb166a8cb76dd712647bcf7505747
Geno Roupsky <geno@roupsky.name>
2017-03-15 12:17:12 +0200

    remove unused import

--- app/src/main/java/org/bspb/smartbirds/pro/ui/MainActivity.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/MainActivity.java
@@ -2,7 +2,6 @@ package org.bspb.smartbirds.pro.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
 import org.androidannotations.annotations.AfterInject;
