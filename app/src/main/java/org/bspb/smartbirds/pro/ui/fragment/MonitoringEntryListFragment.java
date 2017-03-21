980551a00967cf1d8161f4c8bd35b13adc7f1b73
Geno Roupsky <geno@roupsky.name>
2017-03-21 15:43:25 +0200

    delete entry from monitoring

--- app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/MonitoringEntryListFragment.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/MonitoringEntryListFragment.java
@@ -1,16 +1,23 @@
 package org.bspb.smartbirds.pro.ui.fragment;
 
+import android.app.AlertDialog;
 import android.app.ListFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 
 import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
@@ -24,6 +31,8 @@ import org.bspb.smartbirds.pro.content.MonitoringManager;
 import org.bspb.smartbirds.pro.enums.EntryType;
 import org.bspb.smartbirds.pro.ui.partial.MonitoringEntryListRowPartialView;
 import org.bspb.smartbirds.pro.ui.partial.MonitoringEntryListRowPartialView_;
+
+import java.util.Locale;
 
 
 /**
@@ -39,6 +48,9 @@ public class MonitoringEntryListFragment extends ListFragment implements Monitor
 
     @Bean
     MonitoringCursorEntries entries;
+
+    @Bean
+    MonitoringManager monitoringManager;
 
     private String monitoringCode;
 
@@ -75,11 +87,69 @@ public class MonitoringEntryListFragment extends ListFragment implements Monitor
         if (adapter != null) adapter.swapCursor(entries.getCursor());
     }
 
+    @AfterViews
+    protected void setupListview() {
+        final ListView lv = getListView();
+        lv.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        lv.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                Log.d(TAG, String.format(Locale.ENGLISH, "onItemCheckedStateChanged: %d %s", position, checked));
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                Log.d(TAG, String.format(Locale.ENGLISH, "onCreateActionMode: %s", mode));
+                getActivity().getMenuInflater().inflate(R.menu.listselection, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(final ActionMode mode, MenuItem item) {
+                Log.d(TAG, String.format(Locale.ENGLISH, "onActionItemClicked: %s", item));
+                switch (item.getItemId()) {
+                    case R.id.action_select_all:
+                        for (int i = 0; i < lv.getCount(); i++)
+                            lv.setItemChecked(i, true);
+                        return true;
+                    case R.id.action_delete:
+                        final long[] selectedItems = lv.getCheckedItemIds();
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                        builder.setMessage(getString(R.string.confirm_delete_n, selectedItems.length));
+                        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mode.finish();
+                                monitoringManager.deleteEntries(selectedItems);
+                            }
+                        });
+                        builder.setNegativeButton(android.R.string.cancel, null);
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                        return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                Log.d(TAG, String.format(Locale.ENGLISH, "onDestroyActionMode: %s", mode));
+            }
+        });
+    }
+
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         MonitoringEntry entry = (MonitoringEntry) getListAdapter().getItem(position);
-        Log.d(TAG, "entry = " + entry);
         if (entry != null && getActivity() instanceof Listener) {
             ((Listener) getActivity()).onMonitoringEntrySelected(entry.id, entry.type);
         }
