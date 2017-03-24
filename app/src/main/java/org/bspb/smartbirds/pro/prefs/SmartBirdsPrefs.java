70d19ba099b92633e3602bf6f2834090e206e1cd
Geno Roupsky <geno@roupsky.name>
2017-03-24 15:09:19 +0200

    default disply only map

--- app/src/main/java/org/bspb/smartbirds/pro/prefs/SmartBirdsPrefs.java
+++ app/src/main/java/org/bspb/smartbirds/pro/prefs/SmartBirdsPrefs.java
@@ -4,7 +4,7 @@ import org.androidannotations.annotations.sharedpreferences.DefaultBoolean;
 import org.androidannotations.annotations.sharedpreferences.DefaultInt;
 import org.androidannotations.annotations.sharedpreferences.SharedPref;
 
-import static org.bspb.smartbirds.pro.ui.utils.Constants.VIEWTYPE_COMBINED;
+import static org.bspb.smartbirds.pro.ui.utils.Constants.VIEWTYPE_MAP;
 
 /**
  * Created by dani on 14-11-12.
@@ -24,6 +24,6 @@ public interface SmartBirdsPrefs {
     @DefaultBoolean(false)
     boolean stayAwake();
 
-    @DefaultInt(VIEWTYPE_COMBINED)
+    @DefaultInt(VIEWTYPE_MAP)
     int viewType();
 }
