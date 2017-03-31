caf6c6139e276fb04c3f5562f389be400b2c1ca6
Geno Roupsky <geno@roupsky.name>
2017-03-31 14:15:55 +0300

    correct scope for common prefs

--- app/src/main/java/org/bspb/smartbirds/pro/prefs/CommonPrefs.java
+++ app/src/main/java/org/bspb/smartbirds/pro/prefs/CommonPrefs.java
@@ -5,7 +5,7 @@ import org.androidannotations.annotations.sharedpreferences.SharedPref;
 /**
  * Created by groupsky on 22.03.16.
  */
-@SharedPref
+@SharedPref(SharedPref.Scope.APPLICATION_DEFAULT)
 public interface CommonPrefs {
 
     String commonOtherObservers();
