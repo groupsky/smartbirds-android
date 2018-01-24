1e8dd263a1c1fed6b036c8cc6a344300a4cbf034
Daniel Petrov <dani@code6.ninja>
2018-01-24 09:46:18 +0200

    create inveretebrates form

---
+++ app/src/main/java/org/bspb/smartbirds/pro/forms/convert/InvertebratesConverter.java
@@ -0,0 +1,22 @@
+package org.bspb.smartbirds.pro.forms.convert;
+
+import android.content.Context;
+
+import org.bspb.smartbirds.pro.R;
+
+public class InvertebratesConverter extends Converter {
+
+    public InvertebratesConverter(Context context) {
+        super(context);
+
+        addSpecies(R.string.tag_species_scientific_name, "species");
+        addSingle(R.string.tag_sex, "sex");
+        addSingle(R.string.tag_age, "age");
+        addSingle(R.string.tag_habitat, "habitat");
+        addMulti(R.string.tag_threats_other, "threatsInvertebrates");
+        add(R.string.tag_count, "count");
+        add(R.string.tag_marking, "marking");
+        add(R.string.tag_remarks_type, "speciesNotes");
+    }
+
+}
---
+++ app/src/main/java/org/bspb/smartbirds/pro/forms/upload/InvertebratesUploader.java
@@ -0,0 +1,15 @@
+package org.bspb.smartbirds.pro.forms.upload;
+
+import com.google.gson.JsonObject;
+
+import org.bspb.smartbirds.pro.backend.SmartBirdsApi;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+
+public class InvertebratesUploader implements Uploader {
+    @Override
+    public Call<ResponseBody> upload(SmartBirdsApi api, JsonObject data) {
+        return api.createInvertebrates(data);
+    }
+}
