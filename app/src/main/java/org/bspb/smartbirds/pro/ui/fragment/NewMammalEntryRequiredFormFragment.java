e1484f083a16cda813ba53a9f39c4e8fa176b65f
Daniel Petrov <dani@code6.ninja>
2018-02-06 15:07:33 +0200

    autofill confidential field with last value used

--- app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/NewMammalEntryRequiredFormFragment.java
+++ app/src/main/java/org/bspb/smartbirds/pro/ui/fragment/NewMammalEntryRequiredFormFragment.java
@@ -8,9 +8,11 @@ import org.androidannotations.annotations.FragmentById;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 import org.bspb.smartbirds.pro.R;
+import org.bspb.smartbirds.pro.prefs.CommonPrefs_;
 import org.bspb.smartbirds.pro.prefs.MammalPrefs_;
 import org.bspb.smartbirds.pro.ui.views.DecimalNumberFormInput;
 import org.bspb.smartbirds.pro.ui.views.SingleChoiceFormInput;
+import org.bspb.smartbirds.pro.ui.views.SwitchFormInput;
 
 import java.util.HashMap;
 
@@ -27,8 +29,15 @@ public class NewMammalEntryRequiredFormFragment extends BaseFormFragment {
     @ViewById(R.id.form_herp_count)
     DecimalNumberFormInput count;
 
+    @ViewById(R.id.form_mammals_confidential)
+    SwitchFormInput confidential;
+
     @Pref
     MammalPrefs_ prefs;
+
+
+    @Pref
+    CommonPrefs_ commonPrefs;
 
     @FragmentById(R.id.pictures_fragment)
     NewEntryPicturesFragment picturesFragment;
@@ -39,6 +48,7 @@ public class NewMammalEntryRequiredFormFragment extends BaseFormFragment {
         super.onResume();
         if (isNewEntry()) {
             habitat.setText(prefs.mammalHabitat().get());
+            confidential.setChecked(commonPrefs.confidentialRecord().get());
         }
     }
 
@@ -77,6 +87,7 @@ public class NewMammalEntryRequiredFormFragment extends BaseFormFragment {
     public void onPause() {
         super.onPause();
         prefs.mammalHabitat().put(habitat.getText().toString());
+        commonPrefs.confidentialRecord().put(confidential.isChecked());
     }
 
 }
