d1c6729abebf70903c76cc0ffda10bbf814a072d
Daniel Petrov <dani@code6.ninja>
2018-02-22 13:13:54 +0200

    create plants converter

---
+++ app/src/main/java/org/bspb/smartbirds/pro/forms/convert/PlantsConverter.java
@@ -0,0 +1,31 @@
+package org.bspb.smartbirds.pro.forms.convert;
+
+import android.content.Context;
+
+import org.bspb.smartbirds.pro.R;
+
+/**
+ * Created by dani on 22.02.18.
+ */
+
+public class PlantsConverter extends Converter {
+
+    public PlantsConverter(Context context) {
+        super(context);
+
+        // plants
+        addSpecies(R.string.tag_species_scientific_name, "species");
+        add(R.string.tag_elevation, "elevation");
+        addSingle(R.string.tag_habitat, "habitat");
+        addMulti(R.string.tag_accompanyingSpecies, "accompanyingSpecies");
+        addSingle(R.string.tag_reportingUnit, "reportingUnit");
+        addSingle(R.string.tag_phenologicalPhase, "phenologicalPhase");
+        add(R.string.tag_count, "count", "0");
+        add(R.string.tag_density, "density");
+        add(R.string.tag_cover, "cover");
+        addMulti(R.string.tag_threats_other, "threatsPlants");
+        add(R.string.tag_remarks_type, "speciesNotes");
+        addBool(R.string.tag_confidential, "confidential");
+    }
+
+}
