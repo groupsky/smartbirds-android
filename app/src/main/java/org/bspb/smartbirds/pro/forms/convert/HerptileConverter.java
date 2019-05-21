7e45a003b87c74fe166ba7dd8e890f189f58a942
Daniel Petrov <dani@code6.ninja>
2019-05-21 17:46:07 +0300

    Reverse renaming threats_findings field and create new findings field instead

--- app/src/main/java/org/bspb/smartbirds/pro/forms/convert/HerptileConverter.java
+++ app/src/main/java/org/bspb/smartbirds/pro/forms/convert/HerptileConverter.java
@@ -17,7 +17,8 @@ public class HerptileConverter extends Converter {
         addSingle(R.string.tag_sex, "sex");
         addSingle(R.string.tag_age, "age");
         addSingle(R.string.tag_habitat, "habitat");
-        addMulti(R.string.tag_threats_other, "findings");
+        addMulti(R.string.tag_threats_other, "threatsHerptiles");
+        addMulti(R.string.tag_findings, "findings");
         add(R.string.tag_count, "count");
         add(R.string.tag_marking, "marking");
         add(R.string.tag_distance_from_axis, "axisDistance");
