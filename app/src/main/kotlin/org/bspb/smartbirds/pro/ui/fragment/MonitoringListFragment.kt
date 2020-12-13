9a2646e3c16ebe383117f826a24b97e23e57508d
Daniel Petrov <dani@code6.ninja>
2020-12-14 01:58:54 +0200

    Filter monitorings by status

--- app/src/main/kotlin/org/bspb/smartbirds/pro/ui/fragment/MonitoringListFragment.kt
+++ app/src/main/kotlin/org/bspb/smartbirds/pro/ui/fragment/MonitoringListFragment.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.CursorAdapter
 import android.widget.ListView
 import androidx.fragment.app.ListFragment
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
-import org.androidannotations.annotations.AfterViews
-import org.androidannotations.annotations.EFragment
+import org.androidannotations.annotations.*
 import org.bspb.smartbirds.pro.R
 import org.bspb.smartbirds.pro.SmartBirdsApplication
 import org.bspb.smartbirds.pro.adapter.ModelCursorAdapter
@@ -24,8 +22,10 @@ import org.bspb.smartbirds.pro.db.MonitoringColumns
 import org.bspb.smartbirds.pro.db.SmartBirdsProvider.Monitorings
 import org.bspb.smartbirds.pro.ui.partial.MonitoringListRowPartialView
 import org.bspb.smartbirds.pro.ui.partial.MonitoringListRowPartialView_
+import org.bspb.smartbirds.pro.utils.debugLog
 
 @EFragment
+@OptionsMenu(R.menu.monitoring_list)
 open class MonitoringListFragment : ListFragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     companion object {
@@ -37,14 +37,34 @@ open class MonitoringListFragment : ListFragment(), LoaderManager.LoaderCallback
                 MonitoringColumns.ENTRIES_COUNT)
 
         private const val TAG = SmartBirdsApplication.TAG + ".FMonLst"
+        private const val KEY_STATUS = "status"
     }
 
+    private var filterStatus: Monitoring.Status? = null
+
+    @OptionsMenuItem(R.id.menu_filter_status_finished)
+    protected lateinit var menuStatusFinished: MenuItem
+
+    @OptionsMenuItem(R.id.menu_filter_status_uploaded)
+    protected lateinit var menuStatusUploaded: MenuItem
+
+    @OptionsMenuItem(R.id.menu_filter_status_canceled)
+    protected lateinit var menuStatusCanceled: MenuItem
+
+    @OptionsMenuItem(R.id.menu_filter_status_paused)
+    protected lateinit var menuStatusPaused: MenuItem
 
     private var adapter: CursorAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
         super.onCreate(savedInstanceState)
+        savedInstanceState?.run {
+            getString(KEY_STATUS)?.apply {
+                filterStatus = Monitoring.Status.valueOf(this)
+            }
+        }
+
         adapter = object : ModelCursorAdapter<Monitoring?>(activity, R.layout.partial_monitoring_list_row, null, ModelCursorFactory { cursor -> MonitoringManager.monitoringFromCursor(cursor) }) {
             override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
                 return MonitoringListRowPartialView_.build(context)
@@ -56,7 +76,6 @@ open class MonitoringListFragment : ListFragment(), LoaderManager.LoaderCallback
             }
         }
         listAdapter = adapter
-        LoaderManager.getInstance(this).restartLoader(0, null, this)
     }
 
     override fun onResume() {
@@ -75,11 +94,18 @@ open class MonitoringListFragment : ListFragment(), LoaderManager.LoaderCallback
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
         Log.d(TAG, "onCreateLoader")
+        var selection: String? = null
+        var selectionArgs: Array<String>? = null
+
+        filterStatus?.apply {
+            selection = MonitoringColumns.STATUS + "=?"
+            selectionArgs = arrayOf(this.name)
+        }
         return CursorLoader(requireContext(),
                 Monitorings.CONTENT_URI,
                 PROJECTION,
-                null,
-                null,
+                selection,
+                selectionArgs,
                 MonitoringColumns._ID + " DESC")
     }
 
@@ -93,6 +119,13 @@ open class MonitoringListFragment : ListFragment(), LoaderManager.LoaderCallback
         adapter!!.swapCursor(null)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        filterStatus?.apply {
+            updateMenuItems(this)
+        }
+    }
+
     interface Listener {
         fun onMonitoringSelected(monitoringCode: String?)
     }
@@ -100,5 +133,63 @@ open class MonitoringListFragment : ListFragment(), LoaderManager.LoaderCallback
     @AfterViews
     protected open fun setupListView() {
         setEmptyText(getText(R.string.emptyList))
+    }
+
+    @OptionsItem(R.id.menu_filter_status_finished)
+    protected open fun filterFinished() {
+        filterByStatus(Monitoring.Status.finished)
+    }
+
+    @OptionsItem(R.id.menu_filter_status_uploaded)
+    protected open fun filterUploaded() {
+        filterByStatus(Monitoring.Status.uploaded)
+    }
+
+    @OptionsItem(R.id.menu_filter_status_paused)
+    protected open fun filterPaused() {
+        filterByStatus(Monitoring.Status.paused)
+    }
+
+    @OptionsItem(R.id.menu_filter_status_canceled)
+    protected open fun filterCanceled() {
+        filterByStatus(Monitoring.Status.canceled)
+    }
+
+    private fun filterByStatus(status: Monitoring.Status) {
+        filterStatus = if (filterStatus == status) {
+            null
+        } else {
+            status
+        }
+
+        updateMenuItems(filterStatus)
+
+        LoaderManager.getInstance(this).restartLoader(0, null, this)
+    }
+
+    private fun updateMenuItems(status: Monitoring.Status?) {
+        menuStatusFinished.setChecked(false)
+        menuStatusUploaded.setChecked(false)
+        menuStatusPaused.setChecked(false)
+        menuStatusCanceled.setChecked(false)
+
+        status?.apply {
+            when (this) {
+                Monitoring.Status.finished ->
+                    menuStatusFinished.setChecked(true)
+                Monitoring.Status.uploaded ->
+                    menuStatusUploaded.setChecked(true)
+                Monitoring.Status.paused ->
+                    menuStatusPaused.setChecked(true)
+                Monitoring.Status.canceled ->
+                    menuStatusCanceled.setChecked(true)
+            }
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_STATUS, filterStatus?.name)
     }
 }
