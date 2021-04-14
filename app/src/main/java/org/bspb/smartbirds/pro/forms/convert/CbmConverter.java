4b40a7b1677e81888cba8cbbe2921901920e52e7
Denislav <denislavpopov46@gmail.com>
2021-04-14 13:09:49 +0300

    The following fields have been removed: Номер участък Първостепенно местообитание Второстепенно местообитание Посещение номер
    TextView with id "error_cbm_too_far" is moved out of <TableRow> as it was taking  half of the space in the table row. This change also helped to keep the look of Zone field in landscape same as before

--- app/src/main/java/org/bspb/smartbirds/pro/forms/convert/CbmConverter.java
+++ app/src/main/java/org/bspb/smartbirds/pro/forms/convert/CbmConverter.java
@@ -13,10 +13,6 @@ public class CbmConverter extends Converter {
     public CbmConverter(Context context) {
         super(context);
 
-        addSingle(R.string.tag_transect_number, "plot");
-        addSingle(R.string.tag_visit_number, "visit");
-        addSingle(R.string.tag_secondary_habitat, "secondaryHabitat");
-        addSingle(R.string.tag_primary_habitat, "primaryHabitat");
         addSingle(R.string.tag_distance, "distance");
         addSpecies(R.string.tag_observed_bird, "species");
         add(R.string.tag_count_subject, "count");
