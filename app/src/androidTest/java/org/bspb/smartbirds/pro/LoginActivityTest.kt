6f3fc47e17e2816d3f4abe4a7437719c5ba72c34
Geno Roupsky <geno@roupsky.name>
2021-05-29 20:49:43 +0300

    Temporary fail a test to check snapshoting feature

--- app/src/androidTest/java/org/bspb/smartbirds/pro/LoginActivityTest.kt
+++ app/src/androidTest/java/org/bspb/smartbirds/pro/LoginActivityTest.kt
@@ -66,6 +66,7 @@ class LoginActivityTest {
             fieldUsername().perform(typeText("user@smartbirds.org"))
             fieldPassword().perform(typeText("Secret1!"), closeSoftKeyboard())
             buttonLogin().perform(scrollTo(), click())
+            isDisplayed()
         }
 
         mainScreen {
