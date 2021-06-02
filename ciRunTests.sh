7a87a604b0aac71452e816695eb5921217fa01e1
Geno Roupsky <geno@roupsky.name>
2021-06-02 22:06:08 +0300

    Fix check succeeds when tests fail

--- ciRunTests.sh
+++ ciRunTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # stop on command error and print commands
-set -x
+set -ex
 
 # disable animations
 adb shell settings put global window_animation_scale 0
@@ -12,8 +12,14 @@ adb shell settings put global animator_duration_scale 0
 ./gradlew installDebug
 adb shell appops set org.bspb.smartbirds.pro android:mock_location allow
 
-# run the tests
+# run the tests without bailing on error and save the exit code
+set +e
 ./gradlew connectedCheck --stacktrace
+ret=$?
+set -e
 
 # get the screenshots if any from the emulator so they can be inspected
 adb pull /storage/emulated/0/Android/data/org.bspb.smartbirds.pro/files/Pictures/espresso_screenshots || echo "No screenshots"
+
+# exit with the exit code from tests
+exit ret
