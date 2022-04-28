a74de97c40585a99f9d2224850750d9f242b09bb
Daniel Petrov <dani@code6.ninja>
2022-04-28 15:20:38 +0300

    Wip import kml

--- app/src/main/kotlin/org/bspb/smartbirds/pro/ui/fragment/SettingsFragment.kt
+++ app/src/main/kotlin/org/bspb/smartbirds/pro/ui/fragment/SettingsFragment.kt
@@ -1,14 +1,16 @@
 package org.bspb.smartbirds.pro.ui.fragment
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.preference.ListPreference
-import androidx.preference.MultiSelectListPreference
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.preference.*
 import org.bspb.smartbirds.pro.R
 import org.bspb.smartbirds.pro.tools.DBExporter
 import org.bspb.smartbirds.pro.ui.map.MapProvider
+import org.bspb.smartbirds.pro.utils.debugLog
+import org.bspb.smartbirds.pro.utils.popToast
 import org.bspb.smartbirds.pro.utils.showAlert
+import java.io.File
 
 class SettingsFragment : PreferenceFragmentCompat() {
     private var mapTypePreference: ListPreference? = null
@@ -16,9 +18,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
     private var enabledFormsPreference: MultiSelectListPreference? = null
     private var exportPreference: Preference? = null
 
+    private val pickKml = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        copyFileToExternalStorage(it)
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+        initPreferences()
+        updateMapType(MapProvider.ProviderType.valueOf(providerPreference?.value as String))
+    }
 
+    private fun initPreferences() {
         providerPreference = findPreference("providerType")
         mapTypePreference = findPreference("mapType")
         enabledFormsPreference = findPreference("formsEnabled")
@@ -47,9 +57,43 @@ class SettingsFragment : PreferenceFragmentCompat() {
             return@OnPreferenceClickListener true
         }
 
+        val showKmlPreference: SwitchPreferenceCompat? = findPreference("showUserKml")
+        showKmlPreference?.setOnPreferenceChangeListener { preference, newValue ->
+            debugLog("current: ${(preference as SwitchPreferenceCompat).isChecked}, newValue: $newValue")
+            if (newValue as Boolean) {
+                if (!checkExistingUserKml()) {
+                    requireContext().popToast("Missing file. Should open picker")
+                    pickKml()
+                    return@setOnPreferenceChangeListener false
+                }
+            }
 
+            return@setOnPreferenceChangeListener true
+        }
+    }
 
-        updateMapType(MapProvider.ProviderType.valueOf(providerPreference?.value as String))
+    private fun pickKml() {
+        pickKml.launch("application/vnd.google-earth.kml+xml")
+    }
+
+    private fun checkExistingUserKml(): Boolean {
+        return getKmlFile().exists()
+    }
+
+    private fun copyFileToExternalStorage(uri: Uri) {
+        requireContext().contentResolver.openInputStream(uri)?.use {
+            val kmlFile = getKmlFile()
+            if (kmlFile.exists()) {
+                kmlFile.delete()
+            }
+            it.copyTo(getKmlFile().outputStream())
+        }
+    }
+
+    private fun getKmlFile(): File {
+        val kmlFile = File(requireContext().getExternalFilesDir(null), "user.kml")
+        debugLog("PAth: ${kmlFile.absolutePath}")
+        return kmlFile
     }
 
     private fun updateMapType(provider: MapProvider.ProviderType) {
